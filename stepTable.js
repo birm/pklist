1c0561e598f6ce79fab34ec06535e71d452d30a4
Ryan Birmingham <birm@rbirm.us>
2018-11-07 17:23:06 -0500

    add more utils

--- stepTable.js
+++ stepTable.js
@@ -1,3 +1,13 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 function filterData(data, rules){
   return data.filter(y=>{
     for (rule in rules){
@@ -37,8 +47,25 @@ function filterData(data, rules){
 
 
 class stepTable{
-  constructor(url, options){
+  constructor(url, filters, options){
     this.url = url
+    this.filters = filters
     this.options = options
+    this.urlparam = options.urlparam || "filter"
+    this.state = JSON.parse(encodeURIComponent(getParameterByName(urlparam))) || {}
+    // TODO needs a destination to render
   }
+  // in case we want to generalize later
+  async _get_data(url){
+    return await fetch(url)
+  }
+  // render the data to the dest
+  _render(display_data){
+    return display_data
+  }
+  // clear the dest
+  _clear(){
+    return 0
+  }
+  // to get the next filter
 }
